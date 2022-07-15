36a4076f3bd354e354d026e088fa270690dcd037
Murilo-Augusto <mu10-2015@hotmail.com>
2022-07-15 20:09:22 -0300

    Feed e descrição

--- src/Lista/index.js
+++ src/Lista/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import styles from '../components/styleFeed'
 
 export default class Lista extends Component {
     constructor(props) {
@@ -14,9 +15,47 @@ export default class Lista extends Component {
     
     render() {
         return (
-            <View>
-                <Text>Feed...</Text>
+            <View style={styles.feedArea}>
+                
+                <View style={styles.profileView}>
+                    <Image 
+                    source={{uri: this.state.feed.imgperfil}}
+                    style={styles.profilePicture}
+                    />
+                    <Text style={styles.userName} > {this.state.feed.nome} </Text>
+                </View>
+
+                <Image 
+                resizeMode='cover'
+                source={{uri: this.state.feed.imgPublicacao}}
+                style={styles.feedImage}
+                />
+
+                <View style={styles.buttonArea}>
+                    <TouchableOpacity>
+                        <Image 
+                        source={require('../img/like.png')}
+                        style={styles.likeIcon}
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.sendButton} >
+                        <Image 
+                        source={require('../img/send.png')}
+                        style={styles.likeIcon}
+                        />
+                    </TouchableOpacity>
+                </View>
+
+                <View style={styles.descriptionView}>
+                    <Text style={styles.descriptionName}> 
+                        {this.state.feed.nome} 
+                    </Text>
+                    <Text style={styles.descriptionText}> 
+                        {this.state.feed.descricao} 
+                    </Text>
+                </View>
             </View>
         );
     }
 }
+
---
+++ src/components/styleFeed.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { StyleSheet } from 'react-native';
+
+
+const styles = StyleSheet.create({
+    feedArea: {
+
+    },
+    profileView: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 8,
+    },
+    profilePicture: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+    },
+    userName: {
+        fontSize: 22,
+        textAlign: 'left',
+        color: '#000',
+    },
+    feedImage: {
+        flex: 1,
+        height: 400,
+        alignItems: 'center'
+    },
+    buttonArea: {
+        flexDirection: 'row',
+        padding: 5,
+
+    },
+    sendButton: {
+        paddingLeft: 8,
+    },
+    likeIcon: {
+        width: 33,
+        height: 33, 
+    },
+    descriptionView: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    descriptionName: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#000',
+        paddingLeft: 8,
+
+    },
+    descriptionText: {
+        paddingLeft: 8,
+        fontSize: 15,
+        color: '#000',
+    },
+    
+});
+
+export default styles;
